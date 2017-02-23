11710f573578a086988324a5dfc3ad6c19d9767f
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-02-23 09:26:33 -0500

    Add files via upload

---
+++ autoinstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# tells terminal we're using bash cell
+echo hello world
+# prints out “hello world” in the terminal
+#  chmod 700 (filename.sh), to convert to executable 
+# ./(filename.sh), to run it 
+
+
+sudo open HipChat-4.27.0-702.dmg
+sudo umount /Volumes/HipChat-4.27.0-702.dmg
